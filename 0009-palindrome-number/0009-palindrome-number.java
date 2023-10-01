ca13bef9f26cbfbcc3efe176c47a9a258b2cc713
Akansha Verma <120491297+akansha-1202@users.noreply.github.com>
2023-10-01 14:18:13 +0530

    Time: 4 ms (100.00%), Space: 43.3 MB (12.59%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,6 +1,7 @@
 class Solution {
     public boolean isPalindrome(int x) {
-       if(x < 0) return false;
+        if(x >= 0 && x <= 9) return true;
+        if(x < 0 || x %10 == 0) return false;
         
         int temp = x;
         int sum = 0;
