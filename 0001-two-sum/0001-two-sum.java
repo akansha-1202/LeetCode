3e2e4f90edae72fc376a0ec542f50e8b49842385
Akansha Verma <120491297+akansha-1202@users.noreply.github.com>
2023-10-01 13:51:58 +0530

    Time: 38 ms (49.41%), Space: 44 MB (15.82%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -4,10 +4,7 @@ class Solution {
         for(int i = 0; i < n; i++){
             for(int j = i + 1; j < n; j++)
                 if(nums[i] + nums[j] == target){
-                    int[] ans = new int[2];
-                    ans[0] = i;
-                    ans[1] = j;
-                    return ans;
+                    return new int[] {i, j};
                 } 
         }
         return new int[0];
