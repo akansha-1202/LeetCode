d6d75edfb698c7e645fb64cf66d161440048d20a
Akansha Verma <120491297+akansha-1202@users.noreply.github.com>
2023-10-06 13:06:27 +0530

    Time: 1 ms (94.85%), Space: 43.6 MB (31.12%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.java
+++ 0414-third-maximum-number/0414-third-maximum-number.java
@@ -1,12 +1,28 @@
 class Solution {
     public int thirdMax(int[] arr) {
+        boolean flag = false;
         int n = arr.length;
-        Arrays.sort(arr);
-        int dist = 1;
-        for(int i = n - 2; i>= 0; i--) {
-            if(arr[i] != arr[i + 1]) dist++;
-            if(dist == 3) return arr[i];
-        }
-        return arr[n - 1];
+            
+        for(int i = 0; i < n; i++)
+            if(arr[i] == Integer.MIN_VALUE) flag = true;
+        
+        int max = Integer.MIN_VALUE;
+        for(int i = 0; i < n; i++)
+            if(arr[i] > max)
+                max = arr[i];
+        
+        if(n <= 2) return max;
+        int sMax = Integer.MIN_VALUE;
+        for(int i = 0; i < n; i++)
+            if(arr[i] > sMax && arr[i] < max)
+                sMax = arr[i];
+        int tMax = Integer.MIN_VALUE;
+        for(int i = 0; i < n; i++)
+            if(arr[i] > tMax && arr[i] < sMax)
+                tMax = arr[i];
+        if(max == Integer.MIN_VALUE || sMax == Integer.MIN_VALUE) return max;
+        if(tMax != Integer.MIN_VALUE) return tMax;
+        return (tMax == Integer.MIN_VALUE && flag) ? tMax : max;
+        
     }
 }
