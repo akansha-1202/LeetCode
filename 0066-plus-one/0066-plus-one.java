9cdbf93ba8c049783a38b8b7ef348f43eff23c44
Akansha Verma <120491297+akansha-1202@users.noreply.github.com>
2023-10-14 12:07:38 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (15.36%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,13 +1,13 @@
 class Solution {
     public int[] plusOne(int[] digits) {
         int n = digits.length;
-        
-        for(int i = n - 1; i >= 0; i--) {
-            digits[i]++;
+        for(int i = n-1; i >= 0; i--){
+            digits[i]++;    //last digits increment
             if(digits[i] < 10) return digits;
-            digits[i] %= 10;
+            digits[i] %= 10; 
         }
-        int[] ans = new int[n + 1];
+        
+        int[] ans = new int[n+1]; //array size increses
         ans[0] = 1;
         return ans;
     }
