cccb4fb5f101cbb0f065002ef0e2ec70018993ea
Akansha Verma <120491297+akansha-1202@users.noreply.github.com>
2023-10-22 17:07:37 +0530

    Time: 11 ms (11.89%), Space: 49.1 MB (51.15%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -1,10 +1,14 @@
 class Solution {
     public boolean searchMatrix(int[][] arr, int target) {
+        boolean flag = false;
         for(int r = 0; r < arr.length; r++){
             for(int c = 0; c < arr[0].length; c++)
-                if(arr[r][c] == target)
+                if(arr[r][c] == target){
+                     flag = true;
                      return true;
+                }
+            if(flag) break;
         }
-        return false;
+       return false;
     }
 }
