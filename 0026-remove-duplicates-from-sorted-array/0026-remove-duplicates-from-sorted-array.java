2f05d8d8d56eb8dbac6f85d6245e26e4b700efd1
Akansha Verma <120491297+akansha-1202@users.noreply.github.com>
2023-10-23 17:29:45 +0530

    Time: 1 ms (96.62%), Space: 44 MB (30.05%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -3,9 +3,8 @@ class Solution {
         int n = arr.length;
         int i = 0, cnt = 1;
         while(i < n-1){
-            if(arr[i] != arr[i+1]){ 
-                arr[cnt++] = arr[i+1];       
-            } 
+            if(arr[i] != arr[i+1])
+                arr[cnt++] = arr[i+1]; 
             i++;
         }
         return cnt;
